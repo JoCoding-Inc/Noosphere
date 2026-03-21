85928015d087e3be1bcc84c8fe8e6ef4aa09639a
TaeyoungPark1005 <mu07010@jocoding.net>
2026-03-21 23:13:51 +0900

    feat: remove DemoPage, redirect /demo to /

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { HomePage } from './pages/HomePage'
+import { LandingPage }  from './pages/LandingPage'
+import { HomePage }     from './pages/HomePage'
 import { SimulatePage } from './pages/SimulatePage'
-import { ResultPage } from './pages/ResultPage'
-import { HistoryPage } from './pages/HistoryPage'
-import { DemoPage } from './pages/DemoPage'
+import { ResultPage }   from './pages/ResultPage'
+import { HistoryPage }  from './pages/HistoryPage'
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/demo" element={<DemoPage />} />
-        <Route path="/simulate/:simId" element={<SimulatePage />} />
-        <Route path="/result/:simId" element={<ResultPage />} />
-        <Route path="/history" element={<HistoryPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/"                    element={<LandingPage />} />
+        <Route path="/app"                 element={<HomePage />} />
+        <Route path="/demo"                element={<Navigate to="/" replace />} />
+        <Route path="/simulate/:simId"     element={<SimulatePage />} />
+        <Route path="/result/:simId"       element={<ResultPage />} />
+        <Route path="/history"             element={<HistoryPage />} />
+        <Route path="*"                    element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
