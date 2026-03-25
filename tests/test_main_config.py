323601227aeb6d09a2f446902d4b9168f5253093
TaeyoungPark1005 <mu07010@jocoding.net>
2026-03-25 21:39:32 +0900

    chore: remove test suite

--- tests/test_main_config.py
+++
@@ -1,6 +0,0 @@
-def test_simconfig_defaults():
-    from backend.main import SimConfig
-    cfg = SimConfig(input_text="hello")
-    assert cfg.input_text == "hello"
-    assert cfg.num_rounds == 8
-    assert cfg.max_agents == 30
